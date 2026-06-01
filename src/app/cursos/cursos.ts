0716381cfe444a26288105a39afcd25a7d5d87d6
Josema <jose.ames@upch.pe>
2026-06-01 09:40:16 -0500

    Mejora panel administrativo

--- src/app/cursos/cursos.ts
+++ src/app/cursos/cursos.ts
@@ -2,8 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cursos',
+
+  standalone: true,
+
   imports: [],
+
   templateUrl: './cursos.html',
-  styleUrl: './cursos.css',
+
+  styleUrl: './cursos.css'
 })
-export class Cursos {}
+
+export class Cursos {
+
+}
